27c68fe763a69fa625facb006e813682a7ec415d
eklipsorz <eklipsorz@protonmail.ch>
2022-04-24 02:09:33 +0800

    fix: fix admin bug

--- services/user-service.js
+++ services/user-service.js
@@ -11,10 +11,9 @@ const userServices = {
       if (!account || !password) {
         return cb(new APIError({ code: 403, status: 'error', message: '未填寫完所有欄位' }))
       }
-
+      console.log(account, password)
       const user = await User.findOne({ where: { account }, raw: true })
-
-      if (!user || user !== 'admin') {
+      if (!user || user.account === 'admin') {
         return cb(new APIError({ code: 404, status: 'error', message: '帳號不存在' }))
       }
       if (!bcrypt.compareSync(password, user.password)) {
