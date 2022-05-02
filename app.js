d19b0fed0ae7a2393693d273aa7c60e77ebf63ff
eklipsorz <eklipsorz@protonmail.ch>
2022-05-02 13:58:31 +0800

    the first version pushed to gcp

--- app.js
+++ app.js
@@ -3,15 +3,50 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const express = require('express')
-const routes = require('./routes')
-const PORT = process.env.NODE_PORT || 3000
+// const routes = require('./routes')
+
+// const hostname = 'www.belazy.shop'
+const PORT = parseInt(process.env.PORT) || 8080
+// const httpPort = 80
+
+// const httpsOptions = {
+//   cert: fs.readFileSync('./ssl/belazy-shop.crt'),
+//   ca: fs.readFileSync('./ssl/belazy-shop.ca-bundle'),
+//   key: fs.readFileSync('./ssl/belazy-shop.key')
+// }
+
+// const httpServer = http.createServer(app)
+// const httpsServer = https.createServer(httpsOptions, app)
 const app = express()
+// app.use((req, res, next) => {
+//   if (req.protocol === 'http') {
+//     res.redirect(301, `https://${req.headers.host}${req.url}`)
+//   }
+//   return next()
+// })
 
 app.use(express.json())
-
-app.use(routes)
+app.get('/', (req, res) => {
+  res.send('<h1>hi apple</h1>')
+})
+app.get('/.well-known/pki-validation/:something', (req, res) => {
+  const file = `${__dirname}/.well-known/pki-validation/654502BBFBBDD117695EFD0DB6504378.txt`
+  res.download(file) // Set disposition and send it.
+  // res.send('<h1>hi apple</h1>')
+})
+// app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`The express server is running at ${PORT}`)
 })
+
+// httpServer.listen(httpPort, () => {
+//   console.log('http')
+// })
+// httpsServer.listen(httpsPort, () => {
+//   console.log('https')
+// })
