8b7b19ba448db03fdde8de29f6d7146b11912f29
eklipsorz <eklipsorz@protonmail.ch>
2022-05-04 08:10:34 +0800

    refactor: restore from origin version

--- app.js
+++ app.js
@@ -3,24 +3,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const fs = require('fs')
-const http = require('http')
-const https = require('https')
 const express = require('express')
-// const routes = require('./routes')
+const routes = require('./routes')
 
-// const hostname = 'www.belazy.shop'
 const PORT = parseInt(process.env.PORT) || 8080
-// const httpPort = 80
 
-// const httpsOptions = {
-//   cert: fs.readFileSync('./ssl/belazy-shop.crt'),
-//   ca: fs.readFileSync('./ssl/belazy-shop.ca-bundle'),
-//   key: fs.readFileSync('./ssl/belazy-shop.key')
-// }
-
-// const httpServer = http.createServer(app)
-// const httpsServer = https.createServer(httpsOptions, app)
 const app = express()
 // app.use((req, res, next) => {
 //   if (req.protocol === 'http') {
@@ -31,22 +18,11 @@ const app = express()
 
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send(`<h1>hi apple ${process.env.MY_VAR}</h1>`)
+  res.send(`<h1>hi apple ${process.env.MY_VAR} ${process.env.NODE_ENV}</h1>`)
 })
-app.get('/.well-known/pki-validation/:something', (req, res) => {
-  const file = `${__dirname}/.well-known/pki-validation/654502BBFBBDD117695EFD0DB6504378.txt`
-  res.download(file) // Set disposition and send it.
-  // res.send('<h1>hi apple</h1>')
-})
-// app.use(routes)
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`The express server is running at ${PORT}`)
 })
-
-// httpServer.listen(httpPort, () => {
-//   console.log('http')
-// })
-// httpsServer.listen(httpsPort, () => {
-//   console.log('https')
-// })
