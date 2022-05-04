79a6fa88a8cd71bfea770dc62bbbf93d708ea7fd
eklipsorz <eklipsorz@protonmail.ch>
2022-05-04 08:12:50 +0800

    remove additional lines

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send(`<h1>hi apple ${process.env.MY_VAR} ${process.env.NODE_ENV}</h1>`)
 })
-app.get('/.well-known/pki-validation/:something', (req, res) => {
-  const file = `${__dirname}/.well-known/pki-validation/654502BBFBBDD117695EFD0DB6504378.txt`
-  res.download(file) // Set disposition and send it.
-  // res.send('<h1>hi apple</h1>')
-})
+
 // app.use(routes)
 
 app.listen(PORT, () => {
