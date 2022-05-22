9409524b8c0b52f363e81a8f4ad00cbe3a6e16c0
eklipsorz <eklipsorz@protonmail.ch>
2022-05-22 21:14:34 +0800

    fix: rename config name for ssl

--- deploy-tools/deploy-config-file.js
+++ deploy-tools/deploy-config-file.js
@@ -6,9 +6,9 @@ async function main() {
   const keyContent = await FileHandler.readRemoteFile('db-ssl/client-key.pem')
   const certContent = await FileHandler.readRemoteFile('db-ssl/client-cert.pem')
   const caContent = await FileHandler.readRemoteFile('db-ssl/server-ca.pem')
-  fs.writeFileSync('config/ssl/db/key.pem', keyContent)
-  fs.writeFileSync('config/ssl/db/cert.pem', certContent)
-  fs.writeFileSync('config/ssl/db/ca.pem', caContent)
+  fs.writeFileSync('config/ssl/db/client-key.pem', keyContent)
+  fs.writeFileSync('config/ssl/db/client-cert.pem', certContent)
+  fs.writeFileSync('config/ssl/db/server-ca.pem', caContent)
 }
 
 main()
