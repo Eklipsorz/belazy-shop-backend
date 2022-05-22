afb024ceeb918180b7bdee22c8c7e04e6871a1cc
eklipsorz <eklipsorz@protonmail.ch>
2022-05-23 00:24:27 +0800

    feat: restore

--- middlewares/file-uploader.js
+++ middlewares/file-uploader.js
@@ -3,9 +3,7 @@ const { format } = require('util')
 const { DEFAULT_AVATAR, MAXFILESIZE } = require('../config/app').utility.FileUploadToolKit
 const multer = require('multer')
 
-require('dotenv').config()
-const PROD_GCLOUD_STORAGE_BUCKET = process.env.PROD_GCLOUD_STORAGE_BUCKET || 'testawt'
-console.log('PROD_GCLOUD_STORAGE_BUCKET', process.env.PROD_GCLOUD_STORAGE_BUCKET)
+const PROD_GCLOUD_STORAGE_BUCKET = process.env.PROD_GCLOUD_STORAGE_BUCKET
 const storage = new Storage()
 const bucket = storage.bucket(PROD_GCLOUD_STORAGE_BUCKET)
 
