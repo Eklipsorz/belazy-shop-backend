526253d958e78f567f72d0372bc3a2f744101bab
eklipsorz <eklipsorz@protonmail.ch>
2022-05-27 01:14:50 +0800

    feat: test close

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const { project } = require('./config/project')
 require('dotenv').config({ path: project.ENV })
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
-const redisConfig = require('./config/redis')[NODE_ENV]
-const createRedisClient = require('./db/redis/connect-db')
-const client = createRedisClient(redisConfig)
+// const NODE_ENV = process.env.NODE_ENV || 'development'
+// const redisConfig = require('./config/redis')[NODE_ENV]
+// const createRedisClient = require('./db/redis/connect-db')
+// const client = createRedisClient(redisConfig)
 
 const cors = require('cors')
 const express = require('express')
@@ -38,10 +38,10 @@ app.use(express.json())
 //   await client.disconnect()
 //   res.send('disconnected')
 // })
-// app.get('/', (req, res) => {
-//   res.send(`<h1>hi eklipsorz!! this is ${process.env.NODE_ENV} mode</h1>`)
-// })
-// app.use(routes)
+app.get('/', (req, res) => {
+  res.send(`<h1>hi eklipsorz!! this is ${process.env.NODE_ENV} mode</h1>`)
+})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`The express server is running at ${PORT}`)
