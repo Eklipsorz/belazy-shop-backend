e4821b8bcac90f2e98441cfd6295664d4c46324c
eklipsorz <eklipsorz@protonmail.ch>
2022-05-31 23:09:38 +0800

    feat: cancel node-redis connection

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ const PORT = parseInt(process.env.PORT) || 8080
 
 const app = express()
 
-redisClient.connect()
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -44,8 +42,8 @@ app.use(
 // })
 
 app.get('/', async (req, res) => {
-  // req.session.cartId = require('crypto').randomBytes(16).toString('hex')
-  // console.log(req.session)
+  req.session.cartId = require('crypto').randomBytes(16).toString('hex')
+  console.log(req.session)
   res.send(`<h1>hi eklipsorz!! this is ${process.env.NODE_ENV} mode</h1>`)
 })
 
