12e906013bae5b7bd6a45cb87ba34fe1b289bd47
eklipsorz <eklipsorz@protonmail.ch>
2022-06-08 15:24:14 +0800

    test: print debug msg

--- db/redis/index.js
+++ db/redis/index.js
@@ -2,6 +2,7 @@ const Redis = require('ioredis')
 
 function createRedisClient({ username, password, host, port }) {
   const url = `redis://${username}:${password}@${host}:${port}`
+  console.log('result url', url)
   const redis = new Redis(url)
   return redis
 }
